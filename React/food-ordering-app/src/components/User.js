f5bb7441032607bf756c83f39e8e9a5ec55c03cf
mohd-mustufa <mohammedmustufa02@gmail.com>
2023-09-10 22:42:10 +0530

    Played with all lifecycle methods

--- React/food-ordering-app/src/components/User.js
+++ React/food-ordering-app/src/components/User.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { json } from "react-router-dom";
 
 class User extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      userInfo: {
+        name: "Default",
+        bio: "NA",
+      },
       count: 0,
-      count2: 0,
     };
     console.log(this.props.name + "Child Constructor");
   }
@@ -15,32 +17,45 @@ class User extends React.Component {
   async fetchApiData() {
     const data = await fetch("https://api.github.com/users/mohd-mustufa");
     const jsonData = await data.json();
-    console.log(jsonData);
+    this.setState({ userInfo: jsonData });
   }
 
   componentDidMount() {
     console.log(this.props.name + "Child ComponentDidMount");
     this.fetchApiData();
+    this.timer = setInterval(() => {
+      console.log("mount timer");
+    }, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log(this.props.name + "Child ComponentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    console.log(this.props.name + "Child ComponentWillUnmount");
   }
 
   render() {
+    const { name, bio, avatar_url } = this.state.userInfo;
     console.log(this.props.name + "Child Render");
     return (
-      <div className="user">
-        <h2>Count: {this.state.count}</h2>
+      <>
+        <h1>Count: {this.state.count}</h1>
         <button
           onClick={() => {
-            this.setState({ count: this.state.count + 1 }, () =>
-              console.log(this.state)
-            );
+            this.setState({ count: this.state.count + 1 });
           }}
         >
           Increment Count
         </button>
-        <h3>Name: {this.props.name}</h3>
-        <p>location: {this.props.location}</p>
-        <p>Age: 55</p>
-      </div>
+        <div className="user">
+          <img style={{ height: "200px", width: "200px" }} src={avatar_url} />
+          <h3>Name: {name}</h3>
+          <p>Occupation: {bio}</p>
+        </div>
+      </>
     );
   }
 }
